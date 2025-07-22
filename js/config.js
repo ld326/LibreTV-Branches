2eb714befd1481b2ee0213cafb694fd520e9323b
ld326 <63453228+ld326@users.noreply.github.com>
2025-07-22 23:52:14 +0800

    tv190 config.js

--- js/config.js
+++ js/config.js
@@ -13,8 +13,8 @@ const PASSWORD_CONFIG = {
 
 // 网站信息配置
 const SITE_CONFIG = {
-    name: 'LibreTV',
-    url: 'https://libretv.is-an.org',
+    name: 'TV190',
+    url: 'https://TV190.COM',
     description: '免费在线视频搜索与观看平台',
     logo: 'image/logo.png',
     version: '1.0.3'
